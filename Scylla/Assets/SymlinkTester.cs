1c96dc186172fd4487ea5df6aa712d8719bb8bc2
Nicolas Serf <serf.nicolas@gmail.com>
2020-03-18 23:14:24 +0100

    [MISC] Initial project commit 1. Initial project commit for linking with project 2. Added a single SymlinkTester for test purpose

---
+++ Scylla/Assets/SymlinkTester.cs
@@ -0,0 +1,11 @@
+﻿namespace Scylla
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    public class SymlinkTester : MonoBehaviour
+    {
+        public static int tester = 0;
+    }
+}
