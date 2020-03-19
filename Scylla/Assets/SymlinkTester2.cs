76ea98110180824dc409f22273123fedcb6a72a9
Nicolas Serf <serf.nicolas@gmail.com>
2020-03-19 14:04:25 +0100

    [MISC] Test adding a symlink tester 2

---
+++ Scylla/Assets/SymlinkTester2.cs
@@ -0,0 +1,12 @@
+﻿namespace Scylla
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    public class SymlinkTester2 : MonoBehaviour
+    {
+        public static int tester2 = 0;
+    }
+}
+
