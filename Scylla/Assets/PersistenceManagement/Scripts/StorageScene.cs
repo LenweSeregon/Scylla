aca4aef3923043f80984c02f68f88f5ab3d8df45
Nicolas Serf <serf.nicolas@gmail.com>
2020-05-20 17:23:26 +0200

    [DEV] Working on persistence + pull bat 1. Fix pull bat to rebase on master 2. Remove useless class StorableManager 3. Create StorageScene

---
+++ Scylla/Assets/PersistenceManagement/Scripts/StorageScene.cs
@@ -0,0 +1,13 @@
+﻿namespace Scylla.PersistenceManagement
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    public class StorageScene : MonoBehaviour
+    {
+        [SerializeField] private Scylla.CommonModules.Identification.Guid _guid;
+
+        public string Guid => _guid.GetGuid;
+    }
+}
